ecd4b6bcc15ea7fea7bd8ab63779ab323f255610
pavel.hnelytsia <pavelgnelicya@gmail.com>
2022-11-07 17:22:43 +0200

    // add some example bean configuration with parameters in Constructor

--- src/main/java/org/spring/Test4.java
+++ src/main/java/org/spring/Test4.java
@@ -2,23 +2,16 @@ package org.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test3 {
-
+public class Test4 {
     public static void main(String[] args) {
-
         // creating Spring Container
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext
                         ("applicationContext.xml");
 
-
-        // getting from  Spring Container  object = Dog
-        // Pet is common interface  context.getBean using reflection
-        // set into Person Constructor HardCoreJava Dependencies object/bean
-        // from  Spring Container
-        Pet petDog = context.getBean("myPetDog", Pet.class);
-
-        Person person = new Person(petDog);
+        // In Spring Container we have configuration of this Bean with
+        // parameters in Constructor
+        Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
 
         context.close();
